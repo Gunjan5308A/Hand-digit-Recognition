4c0ebb2df023f52c54c267991c82925413fd88b0
Gunjan5308A <gunjanthapa5308a@gmail.com>
2026-02-20 11:59:02 +0545

    done with model acc is around 70 - 80 percent

--- model/model.py
+++ model/model.py
@@ -1,7 +1,11 @@
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 import numpy as np
-import pandas as pd
+
+#used mnist dataset from scikit learn
+#* This model has 2 layer and has 10 neruron in each layer (excluding input layer)
+#* activation func: ReLu and softmax
+#*  loss function binary cross entropy
 
 mnist = fetch_openml('mnist_784', version=1)
 
@@ -16,8 +20,8 @@ def ReLu(Z):
     return np.maximum(0, Z)
 
 def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-
+    exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+    return exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
 
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, 10))
@@ -29,22 +33,20 @@ def deriv_ReLu(Z):
     return Z > 0
 
 def accuracy_report(y_pred, y_test):    
-    """Generate accuracy report"""
+    y_pred = np.argmax(y_pred, axis=0)
     accuracy = np.mean(y_pred == y_test)
     print("="*50)
     print("ACCURACY REPORT")
     print("="*50)
     print(f"Accuracy: {accuracy:.4f} ({accuracy*100:.2f}%)")
     print(f"Correct Predictions: {np.sum(y_pred == y_test)} / {len(y_test)}")
-    print("="*50)
-
 
 class recog:
     def __init__(self, x, y, lr, epochs):
-        self.w1 = np.random.rand(10, 784)
-        self.b1 = np.random.rand(10, 1)
-        self.w2 = np.random.rand(10, 10)
-        self.b2 = np.random.rand(10, 1)
+        self.w1 = np.random.randn(10, 784) * 0.01
+        self.b1 = np.zeros((10, 1))
+        self.w2 = np.random.randn(10, 10) * 0.01
+        self.b2 = np.zeros((10, 1))
         self.X = x
         self.y = y
         self.lr = lr
@@ -56,35 +58,35 @@ class recog:
         self.Z2 = self.w2 @ self.A1 + self.b2
         self.A2 = softmax(self.Z2)
 
-
     def back_prop(self):
         self.m = self.y.size
         self.one_hot_Y = one_hot(self.y)
         self.dZ2 = self.A2 - self.one_hot_Y
         self.dw2 = 1/self.m * self.dZ2.dot(self.A1.T)
-        self.db2 = 1/self.m * np.sum(self.dZ2)
-        self.dZ1 = self.w2.T @self.dZ2 * deriv_ReLu(self.Z1)
+        self.db2 = 1/self.m * np.sum(self.dZ2, 1)
+        self.dZ1 = self.w2.T @(self.dZ2 * deriv_ReLu(self.Z1))
         self.dw1 = 1/self.m * self.dZ1.dot(self.X.T)
-        self.db1 = 1/self.m * np.sum(self.dZ1)
+        self.db1 = 1/self.m * np.sum(self.dZ1, 1)
 
     def update_param(self):
         self.w1 = self.w1 - self.lr * self.dw1
-        self.b1 = self.b1 - self.lr * self.db1
+        self.b1 = self.b1 - self.lr * np.reshape(self.db1, (10, 1))
         self.w2 = self.w2 - self.lr * self.dw2
-        self.b2 = self.b2 - self.lr * self.db2
+        self.b2 = self.b2 - self.lr * np.reshape(self.db2, (10, 1))
         
+
     def gradient_decent(self):
         for i in range(self.epochs):
             self.forward_pass()
             self.back_prop()
             self.update_param()
-            if i % 10 ==0:
-                print(i)
-                
+            if i % 50 == 0:
+                accuracy_report(self.A2, self.y)         
+                print(f"Epoch: {i} \n")
+
     def parameters(self):
         return self.w1, self.w2, self.b1, self.b2
     
-
     def predict(self, X):
         self.L1 = self.w1 @ X + self.b1
         self.AC1 = ReLu(self.L1)
@@ -92,10 +94,7 @@ class recog:
         self.AC2 = softmax(self.L2)
         return self.AC2
 
-
-model = recog(X_train.T, y_train, lr=0.1, epochs=200)
+print('model test results: ')
+model = recog(X_train.T, y_train, lr=0.1, epochs=500)
 model.gradient_decent()
-y_pred = np.argmax(model.predict(X_test.T), axis=0)
-
-# Generate accuracy report
-accuracy_report(y_pred, y_test)
+print(accuracy_report(model.predict(X_test.T), y_test))
